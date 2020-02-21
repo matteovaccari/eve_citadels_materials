b6ade7f36056f95442d55d2f806136336b03278d
Matteo VACCARI <matteo.vaccari@yahoo.com>
2020-02-21 17:02:42 +0100

    refactored keepstar image size and set Raitaru materials

--- src/Raitaru.java
+++ src/Raitaru.java
@@ -1,5 +1,11 @@
 public class Raitaru {
     public String getMaterials() {
-        return "work in progress";
+        return "Tritanium x 5.400.000" + "\n" + "Pyerite x 1.080.000" + "\n" + "Mexallon x 378.000"
+                + "\n" + "Isogen x 43.200" + "\n" + "Nocxium x 8.100"  + "\n" + "Zydrine x 3.780"
+                + "\n" + "Megacyte x 1.620"  + "\n" + "\n" + "Broadcast Node x 21"
+                + "\n" + "Integrity Response Drones x 24" + "\n" + "Nano-Factory x 37"
+                + "\n" + "Organic Mortar Applicators x 37" + "\n" + "Recursive Computing Module x 27"
+                + "\n" + "Self-Harmonizing Power Core x 31" + "\n" + "Sterile Conduits x 31"
+                + "\n" + "Wetware Mainframe x 23" + "\n" + "\n" + "Or those P2 materials :";
     }
 }
