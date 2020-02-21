730ef29c98a2adf515ae6269e0cd4e4928170261
Matteo VACCARI <matteo.vaccari@yahoo.com>
2020-02-21 15:22:19 +0100

    Astrahus class is almost ready - only P2 materials left

--- src/Astrahus.java
+++ src/Astrahus.java
@@ -1,9 +1,10 @@
 public class Astrahus {
 
     public String getMaterials() {
-        return "Tritanium x 8 550 000" + "\n" + "Pyerite x 1 710 000" + "\n" + "Mexallon x 598 500"
-                + "\n" + "Isogen x 68 400" + "\n" + "Nocxium x 12 825"  + "\n" + "Zydrine x 5 985"
-                + "\n" + "Megacyte x 2 565"  + "\n" + "\n" + "Either those P4 materials :" + "Nano-Factory x 54"
+        return "Tritanium x 8.550.000" + "\n" + "Pyerite x 1.710.000" + "\n" + "Mexallon x 598.500"
+                + "\n" + "Isogen x 68.400" + "\n" + "Nocxium x 12.825"  + "\n" + "Zydrine x 5.985"
+                + "\n" + "Megacyte x 2.565"  + "\n" + "\n" + "Broadcast Node x 73"
+                + "\n" + "Integrity Response Drones x 20" + "\n" + "Nano-Factory x 54"
                 + "\n" + "Organic Mortar Applicators x 55" + "\n" + "Recursive Computing Module x 38"
                 + "\n" + "Self-Harmonizing Power Core x 38" + "\n" + "Sterile Conduits x 74"
                 + "\n" + "Wetware Mainframe x 54" + "\n" + "\n" + "Or those P2 materials :";
