00c2136fb1bce13154ea8c499732bb1e8b93f8d6
Matteo VACCARI <matteo.vaccari@yahoo.com>
2020-02-21 15:31:55 +0100

    Keepstar class is almost ready - only P2 materials left

---
+++ src/Keepstar.java
@@ -0,0 +1,12 @@
+public class Keepstar {
+
+    public String getMaterials(){
+        return "Tritanium x 2.030.400.000" + "\n" + "Pyerite x 406.080.000" + "\n" + "Mexallon x 142.128.000"
+                + "\n" + "Isogen x 16.243.200" + "\n" + "Nocxium x 3.045.600"  + "\n" + "Zydrine x 1.421.280"
+                + "\n" + "Megacyte x 609.120"  + "\n" + "\n" + "Nano-Factory x 12.118"
+                + "\n" + "Organic Mortar Applicators x 12.241" + "\n" + "Recursive Computing Module x 8.600"
+                + "\n" + "Self-Harmonizing Power Core x 8.956" + "\n" + "Integrity Response Drones x 5.287" +
+                "\n" + "Broadcast Node x 15.583" + "\n" + "Sterile Conduits x 15.718" + "\n" + "Wetware Mainframe x 11.801" +
+                "\n" + "\n" + "Or those P2 materials :";
+    }
+}
