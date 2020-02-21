79cfba5b2c88082d0885d31d0fe1c2bc6c281c39
vaccari.matteo <matteo.vaccari@yahoo.com>
2020-02-21 10:38:43 +0100

    added buttons to GUI

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,7 +9,14 @@ public class GUI implements ActionListener {
     JPanel panel;
     JTextArea result_label;
     JButton athanor_button;
+    JButton tatara_button;
     JButton fortizar_button;
+    JButton astrahus_button;
+    JButton keepstar_button;
+    JButton raitaru_button;
+    JButton azbel_button;
+    JButton sotiyo_button;
+    JButton palatine_keepstar_button;
 
     public static void main(String[] args) {
         new GUI();
@@ -17,20 +25,53 @@ public class GUI implements ActionListener {
     public GUI() {
         window = new JFrame("Eve citadels materials");
         window.setVisible(true);
-        window.setSize(250, 700);
+        window.setSize(500, 700);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         panel = new JPanel();
 
+        astrahus_button = new JButton("Astrahus");
+        astrahus_button.addActionListener(this);
+        astrahus_button.setPreferredSize(new Dimension(150,30));
+        fortizar_button = new JButton("Fortizar");
+        fortizar_button.addActionListener(this);
+        fortizar_button.setPreferredSize(new Dimension(150,30));
+        keepstar_button = new JButton("Keepstar");
+        keepstar_button.addActionListener(this);
+        keepstar_button.setPreferredSize(new Dimension(150,30));
+        raitaru_button = new JButton("Raitaru");
+        raitaru_button.addActionListener(this);
+        raitaru_button.setPreferredSize(new Dimension(150,30));
+        azbel_button = new JButton("Azbel");
+        azbel_button.addActionListener(this);
+        azbel_button.setPreferredSize(new Dimension(150,30));
+        sotiyo_button = new JButton("Sotiyo");
+        sotiyo_button.addActionListener(this);
+        sotiyo_button.setPreferredSize(new Dimension(150,30));
         athanor_button = new JButton("Athanor");
         athanor_button.addActionListener(this);
-        //athanor_button.setSize(150,100);
+        athanor_button.setPreferredSize(new Dimension(150,30));
+        tatara_button = new JButton("Tatara");
+        tatara_button.addActionListener(this);
+        tatara_button.setPreferredSize(new Dimension(150,30));
+        palatine_keepstar_button = new JButton("Palatine Keepstar");
+        palatine_keepstar_button.addActionListener(this);
+        palatine_keepstar_button.setPreferredSize(new Dimension(400,30));
 
         result_label = new JTextArea("Click on a citadel" + "\n" + "to see materials");
         result_label.setEditable(false);
         result_label.setBackground(null);
 
+        panel.add(astrahus_button);
+        panel.add(fortizar_button);
+        panel.add(keepstar_button);
+        panel.add(raitaru_button);
+        panel.add(azbel_button);
+        panel.add(sotiyo_button);
         panel.add(athanor_button);
+        panel.add(tatara_button);
+        panel.add(palatine_keepstar_button);
+
         panel.add(result_label);
 
         window.add(panel);
