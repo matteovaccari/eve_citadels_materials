1e311176c536ba8c2b0ed1a9ffd57c17996535fd
Matteo VACCARI <matteo.vaccari@yahoo.com>
2020-02-25 13:29:04 +0100

    added P2 materials needed for Keepstar

--- src/Keepstar.java
+++ src/Keepstar.java
@@ -7,6 +7,20 @@ public class Keepstar {
                 + "\n" + "Organic Mortar Applicators x 12.241" + "\n" + "Recursive Computing Module x 8.600"
                 + "\n" + "Self-Harmonizing Power Core x 8.956" + "\n" + "Integrity Response Drones x 5.287" +
                 "\n" + "Broadcast Node x 15.583" + "\n" + "Sterile Conduits x 15.718" + "\n" + "Wetware Mainframe x 11.801" +
-                "\n" + "\n" + "Or those P2 materials :";
+                "\n" + "\n" + "Or those P2 materials :" + "\n"
+                + "Biocells x 589400" + "  Construction Blocks x 550380"
+                + "\n" + "Consumer Electronics x 480840" + "  Coolant x 480840"
+                + "\n" + "Enriched Uranium x 179120" + "  Fertilizer x 478380"
+                + "\n" + "Genetically Enhanced Livestock x 179120" + "  Livestock x 550380"
+                + "\n" + "Mechanical Parts x 350560"
+                + "\n" + "Nanites x 408020" + "  Microfiber Shielding x 490780"
+                + "\n" + "Oxides x 350560" + "  Polyaramids x 490780"
+                + "\n" + "Polytextiles x 348100" + "  Rocket Fuel x 179120"
+                + "\n" + "Silicate Glass x 490780" + "  Superconductors x 348100"
+                + "\n" + "Supertensile Plastics x 589400" + "  Synthetic Oil x 478380"
+                + "\n" + "Test Cultures x 408020" + "  Transmitter x 589400"
+                + "\n" + "Viral Agent x 420100" + "  Water-Cooled CPU x 408020"
+                + "\n" + "Miniature Electronics x 420100" + "  Water x 628720"
+                + "\n" + "Bacteria x 489640" + "  Reactive Metals x 484720" + "\n";
     }
 }
