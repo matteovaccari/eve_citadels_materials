f60146dae6becdde1daf3d1f5cfc17ed06fbf2c3
Matteo VACCARI <matteo.vaccari@yahoo.com>
2020-02-25 15:17:40 +0100

    added Sotiyo and tatara P2 materials

--- src/Sotiyo.java
+++ src/Sotiyo.java
@@ -6,6 +6,20 @@ public class Sotiyo {
                 + "\n" + "Integrity Response Drones x 1.222" + "\n" + "Nano-Factory x 1.857"
                 + "\n" + "Organic Mortar Applicators x 1.403" + "\n" + "Recursive Computing Module x 1.244"
                 + "\n" + "Self-Harmonizing Power Core x 969" + "\n" + "Sterile Conduits x 1.404"
-                + "\n" + "Wetware Mainframe x 1.007" + "\n" + "\n" + "Or those P2 materials :";
+                + "\n" + "Wetware Mainframe x 1.007" + "\n" + "\n" + "Or those P2 materials :" + "\n"
+                + "Biocells x 69160" + "  Construction Blocks x 48220"
+                + "\n" + "Consumer Electronics x 48200" + "  Coolant x 48200"
+                + "\n" + "Enriched Uranium x 19380" + "  Fertilizer x 57280"
+                + "\n" + "Genetically Enhanced Livestock x 19380" + "  Livestock x 48220"
+                + "\n" + "Mechanical Parts x 52500"
+                + "\n" + "Nanites x 45020" + "  Microfiber Shielding x 39220"
+                + "\n" + "Oxides x 52500" + "  Polyaramids x 39220"
+                + "\n" + "Polytextiles x 61580" + "  Rocket Fuel x 19380"
+                + "\n" + "Silicate Glass x 39220" + "  Superconductors x 61580"
+                + "\n" + "Supertensile Plastics x 69160" + "  Synthetic Oil x 57280"
+                + "\n" + "Test Cultures x 45020" + "  Transmitter x 69160"
+                + "\n" + "Viral Agent x 52520" + "  Water-Cooled CPU x 45020"
+                + "\n" + "Miniature Electronics x 52520" + "  Water x 56160"
+                + "\n" + "Bacteria x 56120" + "  Reactive Metals x 74280" + "\n";
     }
 }
--- src/Tatara.java
+++ src/Tatara.java
@@ -6,6 +6,20 @@ public class Tatara {
                 + "\n" + "Integrity Response Drones x 317" + "\n" + "Nano-Factory x 355"
                 + "\n" + "Organic Mortar Applicators x 456" + "\n" + "Recursive Computing Module x 232"
                 + "\n" + "Self-Harmonizing Power Core x 466   " + "\n" + "Sterile Conduits x 359"
-                + "\n" + "Wetware Mainframe x 428" + "\n" + "\n" + "Or those P2 materials :";
+                + "\n" + "Wetware Mainframe x 428" + "\n" + "\n" + "Or those P2 materials :" + "\n"
+                + "Biocells x 16420" + "  Construction Blocks x 15740"
+                + "\n" + "Consumer Electronics x 17680" + "  Coolant x 17680"
+                + "\n" + "Enriched Uranium x 9320" + "  Fertilizer x 15660"
+                + "\n" + "Genetically Enhanced Livestock x 9320" + "  Livestock x 15740"
+                + "\n" + "Mechanical Parts x 15460"
+                + "\n" + "Nanites x 13200" + "  Microfiber Shielding x 14760"
+                + "\n" + "Oxides x 15460" + "  Polyaramids x 14760"
+                + "\n" + "Polytextiles x 13440" + "  Rocket Fuel x 9320"
+                + "\n" + "Silicate Glass x 14760" + "  Superconductors x 13440"
+                + "\n" + "Supertensile Plastics x 16420" + "  Synthetic Oil x 15660"
+                + "\n" + "Test Cultures x 13200" + "  Transmitter x 16420"
+                + "\n" + "Viral Agent x 13520" + "  Water-Cooled CPU x 13200"
+                + "\n" + "Miniature Electronics x 13520" + "  Water x 14360"
+                + "\n" + "Bacteria x 18240" + "  Reactive Metals x 14200" + "\n";
     }
 }
