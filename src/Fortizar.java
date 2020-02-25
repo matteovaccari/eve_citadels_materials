51280222fb8a8d11976a5f7beb364ce0b7490959
Matteo VACCARI <matteo.vaccari@yahoo.com>
2020-02-25 12:47:26 +0100

    added P2 materials needed for Fortizar

--- src/Fortizar.java
+++ src/Fortizar.java
@@ -7,6 +7,20 @@ public class Fortizar {
                 + "\n" + "Integrity Response Drones x 258" + "\n" +"Nano-Factory x 598"
                 + "\n" + "Organic Mortar Applicators x 608" + "\n" + "Recursive Computing Module x 423"
                 + "\n" + "Self-Harmonizing Power Core x 441 " + "\n" + "Sterile Conduits x 783"
-                + "\n" + "Wetware Mainframe x 580" + "\n" + "\n" + "Or those P2 materials :";
+                + "\n" + "Wetware Mainframe x 580" + "\n" + "\n" + "Or those P2 materials :" + "\n"
+                + "Biocells x 29080" + "  Construction Blocks x 27260"
+                + "\n" + "Consumer Electronics x 23760" + "  Coolant x 23760"
+                + "\n" + "Enriched Uranium x 8820" + "  Fertilizer x 23560"
+                + "\n" + "Genetically Enhanced Livestock x 8820" + "  Livestock x 27260"
+                + "\n" + "Mechanical Parts x 17320"
+                + "\n" + "Nanites x 20060" + "  Microfiber Shielding x 24280"
+                + "\n" + "Oxides x 17320" + "  Polyaramids x 24280"
+                + "\n" + "Polytextiles x 17120" + "  Rocket Fuel x 8820"
+                + "\n" + "Silicate Glass x 24280" + "  Superconductors x 17120"
+                + "\n" + "Supertensile Plastics x 29080" + "  Synthetic Oil x 23560"
+                + "\n" + "Test Cultures x 20060" + "  Transmitter x 29080"
+                + "\n" + "Viral Agent x 20820" + "  Water-Cooled CPU x 20060"
+                + "\n" + "Miniature Electronics x 20820" + "  Water x 31320"
+                + "\n" + "Bacteria x 24320" + "  Reactive Metals x 23920" + "\n";
     }
 }
