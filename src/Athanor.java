75b35e4eae86b979bb386ba23c7f48b3c66c77ca
Matteo VACCARI <matteo.vaccari@yahoo.com>
2020-02-27 13:47:19 +0100

    refactored code, EST sell price code is OK

--- src/Athanor.java
+++ src/Athanor.java
@@ -1,17 +1,27 @@
+import java.text.DecimalFormat;
+
 public class Athanor {
 
     public static double estSellPrice;
+    public static double roundedEstSellPrice;
+    public static double estMineralsSellPrice;
+    public static String roundedMineralsSellPrice;
+    public static double estP2SellPrice;
+    public static String roundedP2SellPrice;
+    public static double estP4SellPrice;
+    public static String roundedP4SellPrice;
+    public static String billionUnit;
 
     public String getMaterials() {
 
         return "Tritanium x 7.650.000" + "\n" + "Pyerite x 1.530.000" + "\n" + "Mexallon x 535.500"
                 + "\n" + "Isogen x 61.200" + "\n" + "Nocxium x 11.475" + "\n" + "Zydrine x 5.355"
-                + "\n" + "Megacyte x 2.295" + "\n" + " --> Est.Sell Price(Amarr) = " + getMineralESTprice() + "\n" + "\n"
+                + "\n" + "Megacyte x 2.295" + "\n" + " --> Est.Sell Price(Amarr) = " + getMineralESTprice() + billionUnit + "\n" + "\n"
                 + "Broadcast Node x 21"
                 + "\n" + "Integrity Response Drones x 40" + "\n" + "Nano-Factory x 41"
                 + "\n" + "Organic Mortar Applicators x 50" + "\n" + "Recursive Computing Module x 32"
                 + "\n" + "Self-Harmonizing Power Core x 64    " + "\n" + "Sterile Conduits x 38"
-                + "\n" + "Wetware Mainframe x 50" + "\n" + " --> Est.Sell Price(Amarr) = " + getP4ESTprice() + "\n" + "\n"
+                + "\n" + "Wetware Mainframe x 50" + "\n" + " --> Est.Sell Price(Amarr) = " + getP4ESTprice() + billionUnit + "\n" + "\n"
                 + "Or those P2 materials :"
                 + "\n" + "Biocells x 1860" + "  Construction Blocks x 1760"
                 + "\n" + "Consumer Electronics x 2000" + "  Coolant x 2000"
@@ -27,26 +37,50 @@ public class Athanor {
                 + "\n" + "Viral Agent x 1560" + "  Water-Cooled CPU x 1640"
                 + "\n" + "Miniature Electronics x 1560" + "  Water x 1520"
                 + "\n" + "Bacteria x 2000" + "  Reactive Metals x 1640" + "\n"
-<<<<<<< HEAD
-                + " --> Est.Sell Price(Amarr) = " + getP2ESTprice();
-=======
-                + " --> Est.Price(Amarr) = " + getP2ESTprice() + "\n" + "1 x Athanor --> Est.Price = " + getItemESTprice();
->>>>>>> 22d3c9313f763cab6eb5204ad756ffb08da89450
+                + " --> Est.Sell Price(Amarr) = " + getP2ESTprice() + billionUnit
+                + "\n" + "1 x Athanor --> Est.Price = " + getItemESTprice() + billionUnit;
     }
 
-    public int getP4ESTprice() {
-        return 0;
+    public String getP4ESTprice() {
+        //Reduce size to XXX.X... Million ISK
+        estP4SellPrice = estP4SellPrice / 1000000;
+        //Put price value into String
+        roundedP4SellPrice = String.valueOf(estP4SellPrice);
+        //Reduce decimals numbers
+        roundedP4SellPrice = roundedP4SellPrice.substring(0, Math.min(roundedP4SellPrice.length(), 5));
+        billionUnit  = " Million ISK";
+        return roundedP4SellPrice;
     }
 
-    public int getP2ESTprice() {
-        return 0;
+    public String getP2ESTprice() {
+        //Reduce size to XXX.X... Million ISK
+        estP2SellPrice = estP2SellPrice / 1000000;
+        //Put price value into String
+        roundedP2SellPrice = String.valueOf(estP2SellPrice);
+        //Reduce decimals numbers
+        roundedP2SellPrice = roundedP2SellPrice.substring(0, Math.min(roundedP2SellPrice.length(), 5));
+        billionUnit  = " Million ISK";
+        return roundedP2SellPrice;
     }
 
-    public double getMineralESTprice() {
-        return 0;
+    public String getMineralESTprice() {
+       //Reduce size to XXX.X... Million ISK
+        estMineralsSellPrice = estMineralsSellPrice / 1000000;
+        //Put price value into String
+        roundedMineralsSellPrice = String.valueOf(estMineralsSellPrice);
+        //Reduce decimals numbers
+        roundedMineralsSellPrice = roundedMineralsSellPrice.substring(0, Math.min(roundedMineralsSellPrice.length(), 5));
+            billionUnit  = " Million ISK";
+        return roundedMineralsSellPrice;
     }
     public double getItemESTprice() {
-        return Math.round(estSellPrice);
+        roundedEstSellPrice = Math.round(estSellPrice);
+        if (roundedEstSellPrice >= 1000000000) {
+            billionUnit = " Billion ISK";
+            return roundedEstSellPrice / 1000000000;
+        } else
+            billionUnit  = " Million ISK";
+        return roundedEstSellPrice;
     }
 }
 
--- src/GUI.java
+++ src/GUI.java
@@ -9,6 +9,7 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class GUI implements ActionListener {
@@ -56,108 +57,31 @@ public class GUI implements ActionListener {
     JLabel current_picture;
     String current_picture_name = "empty";
 
-    private static HttpURLConnection con;
+    private static HttpURLConnection athanorItemCon;
+    private static HttpURLConnection athanorMineralsCon;
     String url;
     String urlParameters;
     URL urlObject;
+    boolean isAthanorRequestAlreadyDone = false;
 
     public static void main(String[] args) throws IOException {
         new GUI();
     }
 
     public GUI() throws IOException {
-        window = new JFrame("Eve citadels materials");
-        window.setVisible(true);
-        window.setSize(620, 950);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(false);
 
-        fortizar_image = ImageIO.read(new File("F:/Programmes/projects/eve_citadels_materials/images/fortizar.jpg"));
-        fortizar_picture = new JLabel(new ImageIcon(fortizar_image));
-        astrahus_image = ImageIO.read(new File("F:/Programmes/projects/eve_citadels_materials/images/astrahus.jpg"));
-        astrahus_picture = new JLabel(new ImageIcon(astrahus_image));
-        keepstar_image = ImageIO.read(new File("F:/Programmes/projects/eve_citadels_materials/images/keepstar.jpg"));
-        keepstar_picture = new JLabel(new ImageIcon(keepstar_image));
-        raitaru_image = ImageIO.read(new File("F:/Programmes/projects/eve_citadels_materials/images/raitaru.jpg"));
-        raitaru_picture = new JLabel(new ImageIcon(raitaru_image));
-        azbel_image = ImageIO.read(new File("F:/Programmes/projects/eve_citadels_materials/images/azbel.jpg"));
-        azbel_picture = new JLabel(new ImageIcon(azbel_image));
-        sotiyo_image = ImageIO.read(new File("F:/Programmes/projects/eve_citadels_materials/images/sotiyo.jpg"));
-        sotiyo_picture = new JLabel(new ImageIcon(sotiyo_image));
-        athanor_image = ImageIO.read(new File("F:/Programmes/projects/eve_citadels_materials/images/athanor.jpg"));
-        athanor_picture = new JLabel(new ImageIcon(athanor_image));
-        tatara_image = ImageIO.read(new File("F:/Programmes/projects/eve_citadels_materials/images/tatara.jpg"));
-        tatara_picture = new JLabel(new ImageIcon(tatara_image));
-        palatineKeepstar_image = ImageIO.read(new File("F:/Programmes/projects/eve_citadels_materials/images/palatine_keepstar.jpg"));
-        palatine_keepstar_picture = new JLabel(new ImageIcon(palatineKeepstar_image));
+        setWindow();
+        setPictures();
         panel = new JPanel();
-
-        astrahus_button = new JButton("Astrahus");
-        astrahus_button.addActionListener(this);
-        astrahus_button.setPreferredSize(new Dimension(150, 30));
-        fortizar_button = new JButton("Fortizar");
-        fortizar_button.addActionListener(this);
-        fortizar_button.setPreferredSize(new Dimension(150, 30));
-        keepstar_button = new JButton("Keepstar");
-        keepstar_button.addActionListener(this);
-        keepstar_button.setPreferredSize(new Dimension(150, 30));
-        raitaru_button = new JButton("Raitaru");
-        raitaru_button.addActionListener(this);
-        raitaru_button.setPreferredSize(new Dimension(150, 30));
-        azbel_button = new JButton("Azbel");
-        azbel_button.addActionListener(this);
-        azbel_button.setPreferredSize(new Dimension(150, 30));
-        sotiyo_button = new JButton("Sotiyo");
-        sotiyo_button.addActionListener(this);
-        sotiyo_button.setPreferredSize(new Dimension(150, 30));
-        athanor_button = new JButton("Athanor");
-        athanor_button.addActionListener(this);
-        athanor_button.setPreferredSize(new Dimension(150, 30));
-        tatara_button = new JButton("Tatara");
-        tatara_button.addActionListener(this);
-        tatara_button.setPreferredSize(new Dimension(150, 30));
-        palatine_keepstar_button = new JButton("Palatine Keepstar");
-        palatine_keepstar_button.addActionListener(this);
-        palatine_keepstar_button.setPreferredSize(new Dimension(150, 30));
-
-        result_label = new JTextArea("Click on a citadel" + "to see materials" + "\n" +
-                "1. Minerals" + "\n" + "2. P4 products" + "\n" + "3. or P2 products");
-        result_label.setEditable(false);
-        result_label.setBackground(null);
-
-        panel.add(astrahus_button);
-        panel.add(fortizar_button);
-        panel.add(keepstar_button);
-        panel.add(raitaru_button);
-        panel.add(azbel_button);
-        panel.add(sotiyo_button);
-        panel.add(athanor_button);
-        panel.add(tatara_button);
-        panel.add(palatine_keepstar_button);
-
-        panel.add(result_label);
+        setButtons();
+        setResult_label();
+        addComponentsToPanel();
 
         window.add(panel);
         window.validate();
-        astrahus = new Astrahus();
-        fortizar = new Fortizar();
-        keepstar = new Keepstar();
-        raitaru = new Raitaru();
-        azbel = new Azbel();
-        sotiyo = new Sotiyo();
-        athanor = new Athanor();
-        tatara = new Tatara();
-        palatineKeepstar = new PalatineKeepstar();
+        instanciateCitadels();
 
-        //post request
-        url =  "https://evepraisal.com/appraisal.json";
-        urlObject = new URL(url);
-        con = (HttpsURLConnection) urlObject.openConnection();
-        //Request header
-        con.setRequestMethod("POST");
-        con.setRequestProperty("User-Agent", "Mozilla/5.0");
-
-
+        setBaseUrl();
 
     }
 
@@ -251,10 +175,12 @@ public class GUI implements ActionListener {
                 panel.add(current_picture);
                 current_picture_name = "athanor_image";
             }
-            try {
-                sendAthanorPostRequest();
-            } catch (IOException e1) {
-                e1.printStackTrace();
+            if (!isAthanorRequestAlreadyDone) {
+                try {
+                    sendAthanorPostRequest();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
             }
             result_label.setText(athanor.getMaterials());
         }
@@ -286,27 +212,193 @@ public class GUI implements ActionListener {
         }
     }
 
-    public String getAthanorUrlParameters(){
-        return "market=amarr&raw_textarea=1 x Athanor&persist=no&format=json";
+    public String getAthanorUrlParameters(String type){
+        String returnValue = "";
+        switch (type) {
+            case "minerals":
+                returnValue = "market=amarr&raw_textarea=Tritanium x 7.650.000\n" +
+                        "Pyerite x 1.530.000\n" +
+                        "Mexallon x 535.500\n" +
+                        "Isogen x 61.200\n" +
+                        "Nocxium x 11.475\n" +
+                        "Zydrine x 5.355\n" +
+                        "Megacyte x 2.295&persist=no&format=json";
+                break;
+            case "P2":
+                returnValue = "market=amarr&raw_textarea=Biocells x 1860  Construction Blocks x 1760\n" +
+                        "Consumer Electronics x 2000  Coolant x 2000\n" +
+                        "Enriched Uranium x 1280  Fertilizer x 1820\n" +
+                        "Genetically Enhanced Livestock x 1280  Livestock x 1760\n" +
+                        "Mechanical Parts x 1800\n" +
+                        "Nanites x 1640  Microfiber Shielding x 1700\n" +
+                        "Oxides x 1800  Polyaramids x 1700\n" +
+                        "Polytextiles x 1620  Rocket Fuel x 1280\n" +
+                        "Silicate Glass x 1700  Superconductors x 1620\n" +
+                        "Supertensile Plastics x 1860  Synthetic Oil x 1820\n" +
+                        "Test Cultures x 1640  Transmitter x 1860\n" +
+                        "Viral Agent x 1560  Water-Cooled CPU x 1640\n" +
+                        "Miniature Electronics x 1560  Water x 1520\n" +
+                        "Bacteria x 2000  Reactive Metals x 1640&persist=no&format=json";
+                break;
+            case "P4":
+                returnValue = "market=amarr&raw_textarea=Broadcast Node x 21\n" +
+                        "Integrity Response Drones x 40\n" +
+                        "Nano-Factory x 41\n" +
+                        "Organic Mortar Applicators x 50\n" +
+                        "Recursive Computing Module x 32\n" +
+                        "Self-Harmonizing Power Core x 64    \n" +
+                        "Sterile Conduits x 38\n" +
+                        "Wetware Mainframe x 50&persist=no&format=json";
+                break;
+            case "item":
+                returnValue = "market=amarr&raw_textarea=1 x Athanor&persist=no&format=json";
+                break;
+        }
+        return returnValue;
     }
 
     public void sendAthanorPostRequest() throws IOException {
-        //Send post request
-        con.setDoOutput(true);
-        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.writeBytes(getAthanorUrlParameters());
-        wr.flush();
-        wr.close();
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+        //Send Item post request
+        athanorItemCon = (HttpsURLConnection) urlObject.openConnection();
+        athanorItemCon.setRequestMethod("POST");
+        athanorItemCon.setRequestProperty("User-Agent", "Mozilla/5.0");
+        athanorItemCon.setDoOutput(true);
+
+        DataOutputStream wrItem = new DataOutputStream(athanorItemCon.getOutputStream());
+        wrItem.writeBytes(getAthanorUrlParameters("item"));
+        wrItem.flush();
+        wrItem.close();
+        BufferedReader inItem = new BufferedReader(
+                new InputStreamReader(athanorItemCon.getInputStream()));
+        String inputLineItem;
+        StringBuilder responseItem = new StringBuilder();
+        while ((inputLineItem = inItem.readLine()) != null) {
+            responseItem.append(inputLineItem);
         }
-        in.close();
-        JSONObject jsonObject = new JSONObject(response.toString());
-        Athanor.estSellPrice = jsonObject.getJSONObject("appraisal").getJSONObject("totals").getDouble("sell");
+        inItem.close();
+        JSONObject jsonObjectItem = new JSONObject(responseItem.toString());
+        Athanor.estSellPrice = jsonObjectItem.getJSONObject("appraisal").getJSONObject("totals").getDouble("sell");
+
+        //Send P2 materials post request
+        athanorMineralsCon = (HttpsURLConnection) urlObject.openConnection();
+        athanorMineralsCon.setRequestMethod("POST");
+        athanorMineralsCon.setRequestProperty("User-Agent", "Mozilla/5.0");
+        athanorMineralsCon.setDoOutput(true);
+        DataOutputStream wrMinerals = new DataOutputStream(athanorMineralsCon.getOutputStream());
+        wrMinerals.writeBytes(getAthanorUrlParameters("minerals"));
+        wrMinerals.flush();
+        wrMinerals.close();
+        BufferedReader inMinerals = new BufferedReader(
+                new InputStreamReader(athanorMineralsCon.getInputStream()));
+        String inputLineMinerals;
+        StringBuilder responseMinerals = new StringBuilder();
+        while ((inputLineMinerals = inMinerals.readLine()) != null) {
+            responseMinerals.append(inputLineMinerals);
+        }
+        inMinerals.close();
+        JSONObject jsonObjectMinerals = new JSONObject(responseMinerals.toString());
+        Athanor.estMineralsSellPrice = jsonObjectMinerals.getJSONObject("appraisal").getJSONObject("totals").getDouble("sell");
+
+
+
+        isAthanorRequestAlreadyDone = true;
+    }
+    public void setPictures() throws IOException {
+        fortizar_image = ImageIO.read(new File("D:/programmes/WorkSpace_2/eve_citadel_materials/images/fortizar.jpg"));
+        fortizar_picture = new JLabel(new ImageIcon(fortizar_image));
+        astrahus_image = ImageIO.read(new File("D:/programmes/WorkSpace_2/eve_citadel_materials/images/astrahus.jpg"));
+        astrahus_picture = new JLabel(new ImageIcon(astrahus_image));
+        keepstar_image = ImageIO.read(new File("D:/programmes/WorkSpace_2/eve_citadel_materials/images/keepstar.jpg"));
+        keepstar_picture = new JLabel(new ImageIcon(keepstar_image));
+        raitaru_image = ImageIO.read(new File("D:/programmes/WorkSpace_2/eve_citadel_materials/images/raitaru.jpg"));
+        raitaru_picture = new JLabel(new ImageIcon(raitaru_image));
+        azbel_image = ImageIO.read(new File("D:/programmes/WorkSpace_2/eve_citadel_materials/images/azbel.jpg"));
+        azbel_picture = new JLabel(new ImageIcon(azbel_image));
+        sotiyo_image = ImageIO.read(new File("D:/programmes/WorkSpace_2/eve_citadel_materials/images/sotiyo.jpg"));
+        sotiyo_picture = new JLabel(new ImageIcon(sotiyo_image));
+        athanor_image = ImageIO.read(new File("D:/programmes/WorkSpace_2/eve_citadel_materials/images/athanor.jpg"));
+        athanor_picture = new JLabel(new ImageIcon(athanor_image));
+        tatara_image = ImageIO.read(new File("D:/programmes/WorkSpace_2/eve_citadel_materials/images/tatara.jpg"));
+        tatara_picture = new JLabel(new ImageIcon(tatara_image));
+        palatineKeepstar_image = ImageIO.read(new File("D:/programmes/WorkSpace_2/eve_citadel_materials/images/palatine_keepstar.jpg"));
+        palatine_keepstar_picture = new JLabel(new ImageIcon(palatineKeepstar_image));
+    }
+
+    public void setButtons(){
+        astrahus_button = new JButton("Astrahus");
+        astrahus_button.addActionListener(this);
+        astrahus_button.setPreferredSize(new Dimension(150, 30));
+        fortizar_button = new JButton("Fortizar");
+        fortizar_button.addActionListener(this);
+        fortizar_button.setPreferredSize(new Dimension(150, 30));
+        keepstar_button = new JButton("Keepstar");
+        keepstar_button.addActionListener(this);
+        keepstar_button.setPreferredSize(new Dimension(150, 30));
+        raitaru_button = new JButton("Raitaru");
+        raitaru_button.addActionListener(this);
+        raitaru_button.setPreferredSize(new Dimension(150, 30));
+        azbel_button = new JButton("Azbel");
+        azbel_button.addActionListener(this);
+        azbel_button.setPreferredSize(new Dimension(150, 30));
+        sotiyo_button = new JButton("Sotiyo");
+        sotiyo_button.addActionListener(this);
+        sotiyo_button.setPreferredSize(new Dimension(150, 30));
+        athanor_button = new JButton("Athanor");
+        athanor_button.addActionListener(this);
+        athanor_button.setPreferredSize(new Dimension(150, 30));
+        tatara_button = new JButton("Tatara");
+        tatara_button.addActionListener(this);
+        tatara_button.setPreferredSize(new Dimension(150, 30));
+        palatine_keepstar_button = new JButton("Palatine Keepstar");
+        palatine_keepstar_button.addActionListener(this);
+        palatine_keepstar_button.setPreferredSize(new Dimension(150, 30));
+    }
+
+    public void setResult_label() {
+        result_label = new JTextArea("Click on a citadel" + "to see materials" + "\n" +
+                "1. Minerals" + "\n" + "2. P4 products" + "\n" + "3. or P2 products");
+        result_label.setEditable(false);
+        result_label.setBackground(null);
+    }
+
+    public void addComponentsToPanel() {
+        panel.add(astrahus_button);
+        panel.add(fortizar_button);
+        panel.add(keepstar_button);
+        panel.add(raitaru_button);
+        panel.add(azbel_button);
+        panel.add(sotiyo_button);
+        panel.add(athanor_button);
+        panel.add(tatara_button);
+        panel.add(palatine_keepstar_button);
+        panel.add(result_label);
+    }
+
+    public void setWindow() {
+        window = new JFrame("Eve citadels materials");
+        window.setVisible(true);
+        window.setSize(620, 950);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+    }
+
+    public void instanciateCitadels() {
+        astrahus = new Astrahus();
+        fortizar = new Fortizar();
+        keepstar = new Keepstar();
+        raitaru = new Raitaru();
+        azbel = new Azbel();
+        sotiyo = new Sotiyo();
+        athanor = new Athanor();
+        tatara = new Tatara();
+        palatineKeepstar = new PalatineKeepstar();
+    }
+
+    public void setBaseUrl() throws MalformedURLException {
+        //Base URL
+        url =  "https://evepraisal.com/appraisal.json";
+        urlObject = new URL(url);
+
     }
 }
 
